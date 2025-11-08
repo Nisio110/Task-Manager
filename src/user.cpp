fca25c43d7cae979288789cdf9df7b23a4e19eff
Nisio110 <oisinlane05@gmail.com>
2025-11-08 17:08:36 +0000

    added implmentations for getters and setters

---
+++ src/user.cpp
@@ -0,0 +1,7 @@
+#include "../include/user.h"
+
+std::string user::getName(){return name;}
+void user::setName(std::string s){name = s;}
+
+std::string user::getPassword(){return password;}
+void user::setPassword(std::string s){password = s;}
