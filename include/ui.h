c456a07a5744ebc72a5609148f12eb42f5e2bba4
Nisio110 <oisinlane05@gmail.com>
2025-11-24 19:51:56 +0000

    ui.h

---
+++ include/ui.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+using std::string;
+void showloginMenu();
+bool LoginUser(string username);
+void showMainMenu();
+bool signupUser(string username);
+void viewtasks();
+void addtasks();
+void edittasks();
+void deletetasks();
+void switchuser();
+void logoutandquit();
+void showloginMenu();
