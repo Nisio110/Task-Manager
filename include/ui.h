9872997ab48c9b720bf143c88e325cde4df316ad
OscarTweed <oscar.tweed1@gmail.com>
2025-12-01 18:29:56 +0000

    implemented ui functions

--- include/ui.h
+++ include/ui.h
@@ -15,7 +15,3 @@ void    edittasks();
 void    deletetasks();
 void    switchuser();
 void    logoutandquit();
-<<<<<<< HEAD
-
-=======
->>>>>>> e5ed04f (Replaced if for switch and added fuctions to .h file)
